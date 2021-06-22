437c1f297e9f66ec8c4a499b66eb09695cacf908
Jotpreet Kaur <jotpreet.17311@gmail.com>
2021-06-22 10:26:10 -0400

    Added setup.py and requirements.txt

--- display_data/admin.py
+++ display_data/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
+print("The page for admin purposes")
---
+++ setup.py
@@ -0,0 +1,10 @@
+# from setuptools import setup, find_packages
+#
+# setup(name='yahoo_finance_api',
+#       version='1.0',
+#       packages=find_packages(),
+#       scripts=['manage.py'])
+
+if __name__ == "__main__":
+    import setuptools
+    setuptools.setup()
